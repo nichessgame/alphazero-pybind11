509667b473300d1abf7964117639e6630bf69bd4
vasilije2448 <73365189+vasilije2448@users.noreply.github.com>
2025-10-24 18:52:15 +0200

    fix constants

--- src/nichess_constants.h
+++ src/nichess_constants.h
@@ -4,16 +4,15 @@ namespace alphazero::nichess_gs {
 
 constexpr const int WIDTH = 8;
 constexpr const int HEIGHT = 8;
-constexpr const int NUM_SQUARES = WIDTH * HEIGHT;
-constexpr const int NUM_MAX_POSSIBLE_MOVES_FOR_PIECE = 28;
+constexpr const int NUM_MAX_POSSIBLE_MOVES_FOR_PIECE = 7*8 + 8; // 7 squares x 8 directions + 8 knight moves
 constexpr const int NUM_MOVES = WIDTH * HEIGHT * NUM_MAX_POSSIBLE_MOVES_FOR_PIECE + 1; // +1 for MOVE_SKIP
-constexpr const int MOVE_SKIP_IDX = WIDTH * HEIGHT * NUM_MAX_POSSIBLE_MOVES_FOR_PIECE; // Last index is reserved for MOVE_SKIP
+constexpr const int MOVE_SKIP_IDX = NUM_MOVES - 1;
 constexpr const int NUM_PLAYERS = 2;
-constexpr const int NUM_SYMMETRIES = 1; // technically it's 2, but only 1 matters
+constexpr const int NUM_SYMMETRIES = 2;
 constexpr const std::array<int, 3> BOARD_SHAPE = {2, HEIGHT, WIDTH};
-// 10 piece types (exluding NO_PIECE, 5 for P1 and 5 for P2)
-// 2 layers per type.
+// 12 piece types (exluding NO_PIECE, 6 for P1 and 6 for P2)
+// 2 layers per type, first indicates whether a piece is alive and second the % of max health points.
 // 2 layers indicating whether it's P1 or P2 turn.
-constexpr const std::array<int, 3> CANONICAL_SHAPE = {10 * 2 + 2, HEIGHT, WIDTH};
+constexpr const std::array<int, 3> CANONICAL_SHAPE = {12 * 2 + 2, HEIGHT, WIDTH};
 
 } // namespace alphazero::nichess_gs
